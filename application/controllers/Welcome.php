b878cc9b3b3979632d73e1a87aa65da06e96a094
Fabio William Conceição <messhias@gmail.com>
2017-04-29 18:18:53 -0300

    template created

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,11 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('template/header');
+		$this->load->view("sections/slider");
+		$this->load->view('sections/why-choose');
+		$this->load->view("sections/want-more");
+		$this->load->view("sections/contact");
+		$this->load->view('template/footer');
 	}
 }
---
+++ application/views/sections/want-more.php
@@ -0,0 +1,25 @@
+<section id="features" class="block">
+        <div class="container">
+            <header><h3>Quer mais?</h3></header>
+            <div class="row">
+                <div class="col-md-4 col-sm-4">
+                    <h4>Faça o download das plantas do empreendimento!</h4>
+                </div><!-- /.col-md-8 -->
+                <div class="col-md-8 col-sm-8">
+                    
+                     <form role="form" id="form-slider">
+                                <div class="form-group">
+                                    <label for="email">Email<em>*</em></label>
+                                    <input type="email" class="form-control" id="email" name="email" required='true'>
+                                </div><!-- /.form-group -->
+                             
+                                <div class="form-group">
+                                    <div id="form-slider-status"></div>
+                                    <button type="submit" id="form-slider-submit" class="btn btn-default">Enviar</button>
+                                </div><!-- /.form-group -->
+                            </form><!-- /#form-slider -->
+
+                </div><!-- /.col-md-8 -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </section><!-- /#features .block -->
