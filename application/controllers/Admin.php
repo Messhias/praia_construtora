74e574d353a5bb7070bbc9cdfe99c9bc501e2bbe
Fabio William <messhias@gmail.com>
2017-05-01 16:53:21 -0300

    admin area updates

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -11,4 +11,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin extends CI_Controller {
     
+    public function index(){
+        $this->load->view('admin/login');
+    }
+    
 }
