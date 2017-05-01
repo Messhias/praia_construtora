5ae604670b200ecc5e91008cc1027c45da45386b
Fabio William Conceicao <messhias@gmail.com>
2017-05-01 13:33:50 -0300

    updates

---
+++ application/controllers/Admin.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * @package	CONTROLLER
+ * @author 	FABIO WILLIAM CONCEIÇÃO
+ * @since 	VERSION 1.0.0
+ * @filesource
+*/
+
+
+class Admin extends CI_Controller {
+    
+}
